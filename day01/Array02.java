c9e8d6038cb04c575a33fead1d87e23062fa5b3b
marcos-j-ferreira <marcos.j.lemes.2004@gmail.com>
2025-05-08 09:22:10 -0300

    Arrays - 100%

--- day01/Array02.java
+++ day01/Array02.java
@@ -1,4 +1,5 @@
 package day01;
+import java.util.*;
 
 class Student{
     public String name;
@@ -12,11 +13,8 @@ class Student{
     public String toString(){
         return name;
     }
-}
 
-public class Array02 {
-    public static void main(String[] args){
-
+    void subclass(){
         Student[] myStudents = new Student[]{
             new Student("marocos"), new Student("luiz")
         };
@@ -24,5 +22,45 @@ public class Array02 {
         for(Student i: myStudents){
             System.out.println(i);
         }
+    }
+
+    void copy(){
+
+        int[] array = {1,2,3,4,5};
+
+        int[] cloneArray = array.clone();
+
+        System.out.println(array == cloneArray);
+
+        for(int i = 0; i < cloneArray.length;i++){
+            System.out.println(cloneArray[i]);
+        }
+
+    }
+}
+
+public class Array02 {
+    public static void main(String[] args){
+
+        int[] arr = {9,5,7,1,3};
+        Arrays.sort(arr);
+        System.out.println(Arrays.toString(arr));
+        // ordenar uma array, e trasnformar ela em string para não usar loops
+
+        int[] binary = {1,2,3,4,5};
+
+        int index = Arrays.binarySearch(binary, 5);
+
+        System.out.println("Index encontrado no index: "+index);
+
+        int [] copy = Arrays.copyOf(binary, index);
+
+        System.out.println(Arrays.toString(copy));
+
+        int[] fill = new int[5];
+
+        Arrays.fill(fill, 7);
+
+        System.out.println(Arrays.toString(fill));
     }
 }
