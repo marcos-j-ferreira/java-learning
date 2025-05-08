8718f3a37cc2fd95857eb61dd23061a5c193fd98
marcos-j-ferreira <marcos.j.lemes.2004@gmail.com>
2025-05-08 11:27:39 -0300

    variáveis em java

--- day02/variaveis.java
+++ day02/variaveis.java
@@ -1,6 +1,8 @@
 package day02;
 
-public class variaveis {
+public class Variaveis {
+
+    // variáveis de instância (ou de classe)
 
     public static int age = 20; 
     // Visível para outras classes 
@@ -16,11 +18,17 @@ public class variaveis {
 
 
     public static void main(String[] args){
+
+
+        // Variáveis locais (dentro de métodos)
+        int idade = 30;
+        String name = "Ana";
+
+
         System.out.println(age);
         System.out.println(nome);
         System.out.println(sexo);
         System.out.println(fullName);
 
     }
-    
 }
