66b9d7a392c2f90fc145b29c39a50495db9d0509
marcos-j-ferreira <marcos.j.lemes.2004@gmail.com>
2025-05-08 09:54:53 -0300

    Strings

---
+++ day02/Strings.java
@@ -0,0 +1,23 @@
+package day02;
+
+public class Strings {
+
+    public static void main(String[] args){
+
+        String firstName = new String("Marocos");
+
+        String  lastName = "Júnior";
+
+        String name = "Marocs";
+
+        name = name.concat(" "+lastName);
+
+        System.out.println(name);
+
+        String words = new String("Sequência de palavras grandes");
+
+        System.out.println(words);
+
+    }
+    
+}
