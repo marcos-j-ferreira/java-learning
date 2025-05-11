7a8c0be3b4b06fa94b65404500d4c2558f302b10
marcos-j-ferreira <marcos.j.lemes.2004@gmail.com>
2025-05-10 23:41:28 -0300

    ambiente

--- day03/project03/Cadastro.java
+++ day03/project03/Cadastro.java
@@ -1,10 +1,38 @@
-interface produtos{
+package day03.project03;
+
+interface Produtos{
     void codigo();
     void nome ();
-    void type();
+    void typee();
     void value();
 }
 
+
+class Comida implements Produto{
+
+    public void codigo(){
+        System.out.println("Hello, World");
+    }
+
+    public void nome(){
+        System.out.println("Hello, World");
+
+        
+    }
+    public void typee(){
+        System.out.println("Hello, World");
+
+        
+    }
+    public void value(){
+        System.out.println("Hello, World");
+    }
+
+}
 public class Cadastro {
+
+    public static void main(String[] args){
+        System.out.println("Hello, World");
+    }
     
 }
